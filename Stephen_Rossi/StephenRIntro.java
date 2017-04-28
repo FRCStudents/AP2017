e28bc86fcf2a2fb17c20109072dd279c75ab2761
Stephen Rossi <SteveR@StephenKRossi.com>
2017-04-28 15:20:23 -0400

    Added short Introduciton file

---
+++ Stephen_Rossi/StephenRIntro.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Stephen Rossi on 4/27/2017.
+ */
+public class StephenRIntro {
+    public static void main(String[] args) {   //This is the main sub, runs on startup
+        String myName = "Stephen Rossi";       //Variable/string myName was just defined
+        String myEmail = "SteveR@stephenkrossi.com";
+        String mySite = "StephenKRossi.com";
+
+        //Below I am outputting and printing multiple strings, some being set variables to console.
+        System.out.println("Hello! My name is " + myName + " and if you need to get in contact me please view " + mySite + " or email me at " + myEmail);
+        System.out.println("\nBelow is some basic help to get you started coding in the java language!  :");
+        //Below I am displaying some very simple tips and parts of the java language
+        System.out.println("\n \nHere we are using \\n  \n to skip to the next line in the console \n while this is all written in the same line/ on one line of code.");
+        System.out.println("\nThe '\\n' in use in java and python is much like the <br> tag in html");
+        System.out.println("\nUsing the \\ before a symbol lets you display those symbols such as \\, \", etc.");
+
+        System.out.println("This may be a bit difficult to understand if ran without the code in front of you, so make sure to check out the Intro.java file! :D");
+    }
+}
