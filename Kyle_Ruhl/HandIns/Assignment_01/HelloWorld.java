3714e9093846d82bc66423be0be1c263266997c5
KyleRuhl <kylethe355@gmail.com>
2017-05-07 22:19:30 -0400

    Adding HelloWorld.java

---
+++ Kyle_Ruhl/HandIns/Assignment_01/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package helloworld;
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+System.out.println("Hello World");
+System.out.println("My Name Is Kyle");
+    }  
+    
+}
+
