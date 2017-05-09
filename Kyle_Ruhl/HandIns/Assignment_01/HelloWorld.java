9dfbb1fe66c036d525ce63374d2a62b2f7103ce9
KyleRuhl <kylethe355@gmail.com>
2017-05-08 23:47:52 -0400

    Updated helloworld.java assignment to run smoothly

--- Kyle_Ruhl/HandIns/Assignment_01/HelloWorld.java
+++ Kyle_Ruhl/HandIns/Assignment_01/HelloWorld.java
@@ -1,9 +1,9 @@
+class helloWorld {
+   
+    public static void main(String[] args) {
+   
+        System.out.println("Hello, World");
+ 
+    }
 
-public class HelloWorld {
-
-        public static void main(String[] args) {
-    
-System.out.println("Hello World");,
-fdsf    }  
-    
 }
