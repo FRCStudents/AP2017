4c64df1e1e809fd8c65abcde929635ab86f1d03b
Kyle <kylethe355@gmail.com>
2017-09-13 14:06:29 -0400

    Adding Door.java

---
+++ Kyle_Ruhl/HandIns/Door_Assignment/Door.java
@@ -0,0 +1,51 @@
+public class Door{
+
+	int Width;
+	int Height;
+	String Color;
+	boolean Opened;
+	
+//Color
+	
+	String getColor(){
+		return Color;
+	}
+	void setColor(String New_Color){
+		Color = New_Color;
+	}
+
+//Height
+	
+	int getHeight(){
+		return Height;
+	}
+	void setHeight(int New_Height){
+		Height = New_Height;
+	}
+
+//Width
+	
+	int getWidth(){
+		return Width;
+	}
+	void setWidth(int New_Width){
+		Width = New_Width;
+	}
+
+//Opened
+	
+
+	boolean getOpened(){
+		return Opened;
+	}
+	void setOpened(boolean New_Opened){
+		Opened = New_Opened;
+	}
+/* String toString
+	public String toString(){
+	return"This Door Does Not Close";
+	}
+*/ 
+
+	}
+
