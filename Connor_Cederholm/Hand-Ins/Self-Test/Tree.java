73fb753f9c7d286286d00c6fd26efbacdb35129e
CjCederholm <connorjcederholm@gmail.com>
2017-09-13 08:12:26 -0400

    Hand-Ins

---
+++ Connor_Cederholm/Hand-Ins/Self-Test/Tree.java
@@ -0,0 +1,15 @@
+class Tree{
+	int Height;
+	String Color;
+
+	int getHeight(){
+		return = Height;
+	}
+	void setHeight(int NowHeight){
+		Height = NowHeight
+	}	
+	String getColor (){
+		return = Height
+	}
+	void se
+}
