14dbab6e1bec13e8d07fd7b53d3d76d7715fe094
Kyle <kylethe355@gmail.com>
2017-09-20 13:37:24 -0400

    Adding Mathematica

--- Kyle_Ruhl/HandIns/Mathematica/MathematicaDriver.java
+++ Kyle_Ruhl/HandIns/Mathematica/MathematicaDriver.java
@@ -3,6 +3,12 @@ public class MathematicaDriver {
 
 	public static void main(String[] argv) {
 		Mathematica m = new Mathematica();
-		System.out.println(m);
+		//C.add(2,5);
+ 		//"Result from add";
+		System.out.println(m .add (2,5));
+		System.out.println(m .subtract (67,34));
+		System.out.println(m .multiply (90,6));
+		System.out.println(m .divide (5,5));
+		System.out.println(m .isDivisibleBy (500,86));
 	}
 }
