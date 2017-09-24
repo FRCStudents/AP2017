e59438fd69b36508db95696e47208be302e64fb3
Efraim Krug <ekrug@foxboroughrcs.org>
2017-09-24 15:46:39 -0400

    adding GetMax - demo for argv

--- MrKrug/APClass/APLineGo.java
+++ MrKrug/APClass/APLineGo.java
@@ -4,7 +4,23 @@ import APLine.APLine;
 public class APLineGo{
   public static void main(String args[]){
     APLine apLine = new APLine(3,4, -25);
-    System.out.println("Is on line: " + apLine.isOnLine(3,4));
-    System.out.println("Slope: " + apLine.getSlope());
+    System.out.println("Line is: 3x + 4y - 25 = 0");
+    System.out.println("Point (3,4) is on line: " + apLine.isOnLine(3,4));
+    System.out.println("And the slope: " + apLine.getSlope());
+
+    APLine apLine2 = new APLine(5,10,-35);
+    System.out.println("Line is: 5x + 10y - 35 = 0");
+    for (int x = 0; x < 20; x++){
+	for (int y = 0; y < 20; y++){
+		if(apLine.isOnLine(x,y)){ 
+			System.out.println("Point (" + x + "," + y + ") is on the line");
+			}
+		//else {
+		//	System.out.println("(" + x + "," + y + ") - nope!");
+		//}
+	}
+    }
+    System.out.println("And the slope: " + apLine2.getSlope());
+ 
   }
 }
---
+++ MrKrug/work/GetMax.java
@@ -0,0 +1,9 @@
+
+class GetMax {
+	public static void main(String[] ab){  
+		int a = Integer.parseInt(ab[0]); 
+		int b = Integer.parseInt(ab[1]);
+		System.out.println((a > b) ? a : b);
+		}
+}
+
