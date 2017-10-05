10b220e9134d1e233f7d9117624726fd47ae28bd
Tom Howells <tomert42@gmail.com>
2017-10-04 20:33:17 -0400

    adding older stuff and then uploading sentenceParserv1

---
+++ Tom_Howells/HandIns/XGUI/TunjiGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+class TunjiGUI{
+    public static void main(String[] args){
+        JFrame theGUI = new JFrame();
+        theGUI.setTitle("GUI");
+        theGUI.setSize(200,100);
+        theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Color purple = new Color(115, 0, 153);
+        JPanel panel = new JPanel();
+        panel.setBackground(purple);
+        Container pane = theGUI.getContentPane();
+        pane.add(panel);
+        theGUI.setVisible(true);
+    }
+}
