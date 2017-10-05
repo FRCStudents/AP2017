85342cfd9b392077ccce9b3532170365310b6e9f
Kyle <kylethe355@gmail.com>
2017-10-05 11:53:45 -0400

    Revisions

---
+++ Kyle_Ruhl/HandIns/WordCounter/SA.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/*TEMPORARY STOP
+
+
+
+public class SA{
+    public static void main(String[] agrv){
+
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Type a sentence");
+    string ans = scanner1.nextInt();
+
+	int length(ans);
+	int indexOf(int '');
+
+
+
+    }
+}
+
