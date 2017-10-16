942e1f7d6337684248aacc890ace2a4e27d2288c
James Dimino <james@Jamess-MacBook-Pro.local>
2017-10-16 10:18:49 -0400

    Adding GoCar

---
+++ James_Dimino/Handins/Assignment 004/GoCar.java
@@ -0,0 +1,81 @@
+public class GoCar{
+    public static void main(String[] argv){
+
+    String Car_Forward = "\n  ____ \n /  \\_\\_ \n -0--0--";
+    String Car_Backward = "\n   ____ \n _/_/  \\ \n --0--0-";
+
+        System.out.println(Car_Forward);
+
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+
+        for(int b = 0; b <= 80; b++){
+            System.out.print("\n");
+        }
+            
+                System.out.println(Car_Forward.replaceAll("\n", "\n\t"));
+
+                try{
+                    Thread.sleep(1000);
+                }
+
+                catch(InterruptedException ex){
+                    Thread.currentThread().interrupt();
+                }
+
+                for(int b = 0; b <= 80; b++){
+                    System.out.print("\n");
+                }
+
+                        System.out.println(Car_Forward.replaceAll("\n", "\n\t\t"));
+
+                        try{
+                            Thread.sleep(1000);
+                        }
+
+                        catch(InterruptedException ex){
+                            Thread.currentThread().interrupt();
+                        }
+
+                        for(int b = 0; b <= 80; b++){
+                            System.out.print("\n");
+                        }
+
+        System.out.println(Car_Backward.replaceAll("\n", "\n\t\t"));
+
+        try{
+            Thread.sleep(1000);
+        }
+
+        catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+
+        for(int b = 0; b <= 80; b++){
+            System.out.print("\n");
+        }
+
+                System.out.println(Car_Backward.replaceAll("\n", "\n\t"));
+
+                try{
+                    Thread.sleep(1000);
+                }
+
+                catch(InterruptedException ex){
+                    Thread.currentThread().interrupt();
+                }
+
+                for(int b = 0; b <= 80; b++){
+                    System.out.print("\n");
+                }
+
+                        System.out.println(Car_Backward);
+
+    }
+}
+
+
