e73f12c90141aa6101ac1c30d91c56144174a513
Julie Hu <hu10julie@gmail.com>
2017-10-17 12:03:24 -0400

    Julie's stuff

---
+++ Julie_Hu/Hand Ins/Assignment010- SA.java/SA.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SA{
+	public static void main(String[] agrv){
+
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Type a sentence");
+	String ans = scanner1.nextInt();
+
+	int length(ans);
+	int indexOf(int '');
+
+
+
+    }
+}
+
