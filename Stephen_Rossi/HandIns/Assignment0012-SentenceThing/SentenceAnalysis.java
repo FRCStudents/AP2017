7e1ec03234b5193adb8712f0d78c3046ffe26f80
Stephen Rossi <stephenrossi99@gmail.com>
2017-10-17 12:15:10 -0400

    Adding Sentence Analyze

---
+++ Stephen_Rossi/HandIns/Assignment0012-SentenceThing/SentenceAnalysis.java
@@ -0,0 +1,33 @@
+public class SentenceAnalysis {
+  private String sentence;
+  private int numOfWords;
+  private double averageWordLength;
+  private int totalCharacters;
+
+  public SentenceAnalysis(String var) {
+    sentence = var;
+  }
+  public int getNumberOfWords() {
+    String[] words = this.sentence.split(" ");
+    this.numOfWords = words.length;
+    return words.length;
+  }
+  public double calcAverageWordLength(){
+    String[] words = this.sentence.split(" ");
+    int sum = 0;
+    for(int i = 0; i < words.length; i ++) {
+      int h = words[i].length();
+      sum = sum +  h;
+    }
+    this.totalCharacters = sum;
+    double averageLength = (double)sum / words.length;
+    this.averageWordLength = averageLength;
+    return averageLength;
+  }
+  public static void main(String[] args){
+    SentenceAnalysis s1 = new SentenceAnalysis("Whats up dad!");
+    s1.getNumberOfWords();
+    s1.calcAverageWordLength();
+    System.out.println("Sentence: " + s1.sentence + " \n NumOfWords:" + s1.numOfWords + "\n totalCharacters:" + s1.totalCharacters + "\n averageWordLength:" + s1.averageWordLength);
+  }
+}
