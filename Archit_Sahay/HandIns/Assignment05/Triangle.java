e65762885c4f37c185918546b0d0041dbee376a9
Archit Sahay <ArchitSahay>
2017-10-18 12:58:37 -0400

    Adding more assignments

---
+++ Archit_Sahay/HandIns/Assignment05/Triangle.java
@@ -0,0 +1,27 @@
+public class Triangle {
+	int side1 = 5;
+	int side2 = 5;
+	int side3 = 5;
+
+	int current_side1 = 5;
+	int getCurrent_side1 () {
+		return current_side1;
+	}
+	void setCurrent_Side1 (int newCurrent_Side1) {
+		current_side1 = newCurrent_Side1;
+	}
+	int current_side2 = 5;
+	int getCurrent_side2 () {
+		return current_side2;
+	}
+	void setCurrent_Side2 (int newCurrent_Side2) {
+		current_side2 = newCurrent_Side2;
+	}
+	int current_side3 = 5;
+	int getCurrent_side3 () {
+		return current_side3;
+	}
+	void setCurrent_Side3 (int newCurrent_Side3) {
+		current_side3 = newCurrent_Side3;
+	}
+}
