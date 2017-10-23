2cf5abfd502db6f2d5fecfc15c9446bc0c85997e
Kyle <kylethe355@gmail.com>
2017-10-23 10:10:47 -0400

    Adding Swimmer Drafts

---
+++ Kyle_Ruhl/HandIns/WorksInProgress/Swimmer/MySwimmerDriver.java
@@ -0,0 +1,12 @@
+class MySwimmerDriver{
+
+Swimmer s1 = new Swimmer();
+Swimmer s2 = new Swimmer();
+final int NUM_Swimmers = 5; //sets the limit of swimmers at 5
+
+private Swimmer[]daRace = new Swimmer[NUM_Swimmers];
+	//daRace[0].getName();
+private int[] lanes = new int[NUM_Swimmers];
+	
+
+}
