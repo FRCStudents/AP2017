02df07a533d835996a86686f34dae59d9c7f4e6c
James Dimino <JamesDimino01@gmail.com>
2017-10-23 09:28:46 -0400

    Adding Swimmer

---
+++ James_Dimino/Handins/011Swimmer/SwimmerDriver.java
@@ -0,0 +1,8 @@
+class SwimmerDriver{
+	public static void main(String[] argv){
+
+		Swimmer x = new Swimmer("Michael Fred Phelps, II", "Male", "USA", "6ft 3.5in", "196 lbs", 32, "TBD", "18 Gold, 2 Silver, 2 Bronze" );
+		System.out.println(x);
+		
+	}
+}
