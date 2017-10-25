dfa8d3f66f332e783fbcd58878f4029e5af02f49
KyleRuhl <kylethe355@gmail.com>
2017-10-24 22:38:36 -0400

    adding SentAna draft

---
+++ Kyle_Ruhl/HandIns/WorksInProgress/WordCounter/SentAnaDriver.java
@@ -0,0 +1,30 @@
+//my driver for SentAna.java
+public class SentAnaDriver{
+
+	public static void main(String[] args){
+		
+		SentAna q = new SentAna();
+		
+		System.out.println();
+		
+		System.out.println("The Sentence is: " + q.sentence);
+		
+		System.out.println();
+		
+		System.out.println("Calculating...");
+		
+		System.out.println();
+		
+		System.out.println("Word Count is: " + q.wordCount);
+		
+		System.out.println();
+		
+		System.out.println("Average Word Size in the sentence is: " + q.averageWordSize);
+		
+		System.out.println();
+	
+		System.out.println("done. Thanks for letting me Analyze your sentence.");
+		
+	}
+
+}
