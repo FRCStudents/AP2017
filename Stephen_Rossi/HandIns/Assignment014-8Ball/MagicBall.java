4bc5cee376651f7104f8a902d0c4058b1a045011
Stephen Rossi <stephenrossi99@gmail.com>
2017-10-26 14:16:34 -0400

    8Ball

---
+++ Stephen_Rossi/HandIns/Assignment014-8Ball/MagicBall.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class MagicBall {
+  private static String[] responses = {"Concentrate and Ask Again", "Don't Count On It", "It IS Certain", "Most Likely", "My Reply is No", "My Sources Say No", "No", "Outlook Good", "Yes, Definitely", "You May Rely On It"};
+  private static String lastQuestion;
+
+  public static void setLastQuestion(String q){
+    lastQuestion = q;
+  }
+  public static String getLastQuestion(){
+    return lastQuestion;
+  }
+  public static int shakeBall(){
+    return (int)Math.ceil(Math.random() * 9);
+  }
+  public static String response(int value){
+    String x = responses[value];
+    return x;
+  }
+  public static void askQuestion() {
+    Scanner q = new Scanner(System.in);
+    System.out.print("What yes or no question would you like to ask the mystical ball of 8... \n >>> ");
+    String question = q.nextLine();
+    setLastQuestion(question);
+  }
+  public static String run8BallOne(){
+    return "The answer to your question of... '" + lastQuestion + "' is: \n     >> ";
+  }
+  public static void wait(int value){
+          try{
+              Thread.sleep(value);
+          }
+          catch(InterruptedException ex) {
+              Thread.currentThread().interrupt();
+          }
+  }
+  public static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+  public static void start(){
+    clearScreen();
+    askQuestion();
+    System.out.print(run8BallOne());
+    wait(500);
+    System.out.print(response(shakeBall()) + "\n\n\n\n");
+    wait(1500);
+  }
+  public static void keepGoing(){
+    Scanner again = new Scanner(System.in);
+    System.out.print("Would you like to ask another question? y/n \n         >");
+    char answer = (char)again.next().charAt(0);
+    if(answer == 'y') {
+      wait(200);
+      start();
+    }else if(answer == 'n'){
+      System.out.print("Have a nice day");
+      for(int i = 0; i < 3; i++){
+          wait(500);
+          System.out.print(".");
+      }
+      System.out.print(" but remember what you were told.");
+      for(int i = 0; i < 4; i++){
+          wait(700);
+          System.out.print(".");
+      }
+      wait(800);
+      clearScreen();
+      System.exit(0);
+    } else {
+      System.out.println("\n\n\n        MISTAKES WERE MADE... Please answer with 'y' or 'n'...  \n\n\n\n");
+      keepGoing();
+    }
+  }
+  public static void main(String[] args) {
+    start();
+    keepGoing();
+  }
+}
--- Stephen_Rossi/Projects/Testing/Testing.java
+++ Stephen_Rossi/Projects/Testing/Testing.java
@@ -1,56 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 
-class TrainDemo {
+class Testing {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Train Demo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 400);
-        frame.setLocationRelativeTo(null);
-        frame.add(new TrainCanvas());
-        frame.setVisible(true);
-    }
-
-}
-
-class TrainCanvas extends JComponent {
-
-    private int lastX = 0;
-
-    public TrainCanvas() {
-        Thread animationThread = new Thread(new Runnable() {
-            public void run() {
-                while (true) {
-                    repaint();
-                    try {Thread.sleep(10);} catch (Exception ex) {}
-                }
-            }
-        });
-
-        animationThread.start();
-    }
-
-    public void paintComponent(Graphics g) {
-        Graphics2D gg = (Graphics2D) g;
-
-        int w = getWidth();
-        int h = getHeight();
-
-        int trainW = 100;
-        int trainH = 10;
-        int trainSpeed = 3;
-
-        int x = lastX + trainSpeed;
-
-        if (x > w + trainW) {
-            x = -trainW;
-        }
-
-        gg.setColor(Color.BLACK);
-        gg.fillRect(x, h/2 + trainH, trainW, trainH);
-
-        lastX = x;
+      int[] x = {1,2,3,4,5,6,7};
+      System.out.println(x);
     }
 
 }
