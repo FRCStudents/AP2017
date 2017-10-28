381bfb7ab521840c13fb814c316689ebdb0fd978
Efraim Krug <ekrug@foxboroughrcs.org>
2017-10-28 19:25:28 -0400

    as is

--- Stephen_Rossi/HandIns/Assignment014-8Ball/MagicBall.java
+++ Stephen_Rossi/HandIns/Assignment014-8Ball/MagicBall.java
@@ -1,31 +1,48 @@
 import java.util.*;
 
 public class MagicBall {
-  private static String[] responses = {"Concentrate and Ask Again", "Don't Count On It", "It IS Certain", "Most Likely", "My Reply is No", "My Sources Say No", "No", "Outlook Good", "Yes, Definitely", "You May Rely On It"};
+  private static String[] responses = {"Concentrate and Ask Again",
+					"Don't Count On It",
+					"It IS Certain",
+					"Most Likely",
+          "My Reply is No",
+          "My Sources Say No",
+          "No",
+          "Outlook Good",
+          "Yes, Definitely",
+          "You May Rely On It"
+        };
+
   private static String lastQuestion;
 
   public static void setLastQuestion(String q){
-    lastQuestion = q;
+      lastQuestion = q;
   }
+
   public static String getLastQuestion(){
     return lastQuestion;
   }
+
   public static int shakeBall(){
     return (int)Math.ceil(Math.random() * 9);
   }
+
   public static String response(int value){
     String x = responses[value];
     return x;
   }
+
   public static void askQuestion() {
     Scanner q = new Scanner(System.in);
     System.out.print("What yes or no question would you like to ask the mystical ball of 8... \n >>> ");
     String question = q.nextLine();
     setLastQuestion(question);
   }
+
   public static String run8BallOne(){
     return "The answer to your question of... '" + lastQuestion + "' is: \n     >> ";
   }
+
   public static void wait(int value){
           try{
               Thread.sleep(value);
@@ -34,9 +51,13 @@ public class MagicBall {
               Thread.currentThread().interrupt();
           }
   }
+
+
   public static void clearScreen() {
       System.out.flush();
-     }
+  }
+
+
   public static void start(){
     clearScreen();
     askQuestion();
@@ -45,32 +66,35 @@ public class MagicBall {
     System.out.print(response(shakeBall()) + "\n\n\n\n");
     wait(1500);
   }
+
   public static void keepGoing(){
     Scanner again = new Scanner(System.in);
     System.out.print("Would you like to ask another question? y/n \n         >");
-    char answer = (char)again.next().charAt(0);
+    char answer = (char)again.next().tolowercase().charAt(0);
     if(answer == 'y') {
       wait(200);
       start();
-    }else if(answer == 'n'){
-      System.out.print("\n\n\n\n     Have a nice day");
-      for(int i = 0; i < 3; i++){
-          wait(500);
-          System.out.print(".");
-      }
-      System.out.print(" but remember what you were told.");
-      for(int i = 0; i < 4; i++){
-          wait(700);
-          System.out.print(".");
-      }
-      wait(800);
-      clearScreen();
-      System.exit(0);
-    } else {
-      System.out.println("\n\n\n        MISTAKES WERE MADE... Please answer with 'y' or 'n'...  \n\n\n\n");
-      keepGoing();
-    }
+    } else 
+        if(answer == 'n'){
+            System.out.print("\n\n\n\n     Have a nice day");
+            for(int i = 0; i < 3; i++){
+                wait(500);
+                System.out.print(".");
+            }
+            System.out.print(" but remember what you were told.");
+            for(int i = 0; i < 4; i++){
+                wait(700);
+                System.out.print(".");
+            }
+        wait(800);
+        clearScreen();
+        System.exit(0);
+        } else {
+            System.out.println("\n\n\n        MISTAKES WERE MADE... Please answer with 'y' or 'n'...  \n\n\n\n");
+            keepGoing();
+        }
   }
+
   public static void main(String[] args) {
     start();
     keepGoing();
