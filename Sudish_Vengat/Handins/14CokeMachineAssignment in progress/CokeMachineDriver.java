a68023f81c489a685f5e59ba213a34bca9c62925
Svengat <sudish.vengat@gmail.com>
2017-10-31 17:56:44 -0400

    Adding Days of the Week Assignment

---
+++ Sudish_Vengat/Handins/14CokeMachineAssignment in progress/CokeMachineDriver.java
@@ -0,0 +1,11 @@
+public class CokeMachineDriver {
+
+	public static void main (String [] args){
+
+		CokeMachine c = new CokeMachine();
+
+			System.out.println(c.moneyForCokeMachine());
+			System.out.println(c.purchaseSprite());
+			System.out.println("COOOOOOOKKKKKKKKKEEEEEEEEEESSSSSSSSSSSSSSSS")
+	}
+}
