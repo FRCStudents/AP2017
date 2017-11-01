60943c0d0041ab2ee824d64932db27f64efe1629
Nicole Sorial <nicolesorial@Nicoles-MacBook-Pro.local>
2017-10-31 21:35:47 -0400

    adding days of the week and sentence editing objects

---
+++ Nicole_Sorial/HandIn/Assign013/SentenceEdit.java
@@ -0,0 +1,91 @@
+// an object that:
+// 1) prints a string
+// 2) filters out the spaces and prints the string
+// 3) increases each letter by two places and prints out the string
+//  **(copy string from one string to another)
+
+import java.util.*;
+
+class SentenceEdit {
+
+	// ATTRIBUTES
+	String original_string = "";
+	String string_no_spaces = "";
+	String string_update_char = "";
+	char[] alphabet = new char[26];
+	Scanner s = new Scanner(System.in);
+	int idx;
+
+	SentenceEdit() {
+		String original_string = "";
+		String string_no_spaces = "";
+		String string_update_char = "";
+		char[] alphabet = new char[26];
+		Scanner s = new Scanner(System.in);
+		int idx = -1;
+	}
+
+	// METHODS
+
+	// creates an array of the chars of the alphabet
+	public void alphabet() {
+		int index = 0;
+		for (char c = 'a'; c <= 'z'; c++) {
+			alphabet[index++] = c;
+		}
+	}
+
+	// accepts a sentence from user input/scanner
+	public String acceptString() {
+		System.out.println("Please input a sentence (no punctuation please!");
+		original_string = s.nextLine();
+		return original_string;
+	}
+
+	public void printString(String print_string) {
+		System.out.println(print_string);
+	}
+
+	// creates new string with no spaces
+	public String filterSpaces() {
+		for (int i = 0; i < original_string.length(); i++) {
+			if (original_string.charAt(i) == ' ') {
+				string_no_spaces = string_no_spaces;
+			} else {
+				string_no_spaces = string_no_spaces + original_string.charAt(i);
+			}
+		}
+		return string_no_spaces;
+	}
+
+	// helper function to set index to be the index of a char in alphabet[]
+	public void setIndex(int x) {
+		idx = x;
+	}
+
+	// changes each char in a string to be two places forward
+	public String updateString() {
+		string_no_spaces = string_no_spaces.toLowerCase();
+		for (int i = 0; i < string_no_spaces.length(); i++) {
+			// makes i usable in inner for loop
+			int x = i; 
+
+			// loops through whole alphabet to find char's index
+			for (int j = 0; j < 26; j++) { 
+				if (string_no_spaces.charAt(x) == alphabet[j]) {
+					setIndex(j); 
+				}
+			}
+
+			// accounts for the end of the alphabet looping back to the beginning
+			if (idx == 24) { 
+				string_update_char = string_update_char + 'a';
+			} else if (idx == 25) {
+				string_update_char = string_update_char + 'b';
+			} else {
+				string_update_char = string_update_char + alphabet[idx + 2];
+			}
+		}
+		return string_update_char;
+	}
+}
