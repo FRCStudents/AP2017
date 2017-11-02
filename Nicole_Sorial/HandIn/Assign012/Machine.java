09b22cff2ab433567370deb64da822f67c8f96b8
Nicole Sorial <nicolesorial@Nicoles-MacBook-Pro.local>
2017-11-02 14:32:13 -0400

    adding CokeMachine

--- Nicole_Sorial/HandIn/Assign012/Machine.java
+++ Nicole_Sorial/HandIn/Assign012/Machine.java
@@ -6,19 +6,19 @@ class Machine {
 	Drink d1 = new Drink("Coca-Cola", 2.50, 5);
 	Drink d2 = new Drink("Sprite", 2.50, 5);
 	Drink d3 = new Drink("Lemonade", 1.50, 5);
-	String currentDrinkCode;
-	Drink currentDrink;
-	double currentInput = 0;
-	double currentPrice = 0;
-	String goOn = "Yes";
-	String currentMoneyType;
+	String currentDrinkCode; // will allow for input in acceptDrink()
+	Drink currentDrink; // used to identify drink, price, and inventory based on user choice
+	double currentInput = 0; // assigned to cent value after coin is inserted
+	double currentPrice = 0; // updates as coins are inserted
+	String goOn = "Yes"; // continues the program until user does not want to buy more
+	String currentMoneyType; // allows user input for coins
 	String quarter = "q"; // not chars so that they can be compared
 	String dime = "d";
 	String nickel = "n";
 	Scanner s = new Scanner(System.in);
 
 	// Methods
-	public void displayOptions() {
+	public void displayOptions() { // will not display option if there are no more
 		System.out.println("Welcome to the Coke Machine!");
 		System.out.println("We have:");
 		if (d1.inventory > 0) {
@@ -32,7 +32,7 @@ class Machine {
 		}
 	}
 
-	public void acceptDrink() {
+	public void acceptDrink() { // lets user choose a drink then uses determineDrink()
 		if (d1.inventory > 0) {
 			System.out.println("To buy " + d1.name + ", please enter d1");
 		}
@@ -46,7 +46,7 @@ class Machine {
 		determineDrink();
 	}
 
-	public void determineDrink() {
+	public void determineDrink() { // sets currentDrink and currentPrice based on user input
 		if (d1.inventory > 0 && currentDrinkCode.equals("d1")) {
 			currentDrink = d1;
 			currentPrice = d1.price;
@@ -62,7 +62,7 @@ class Machine {
 		}
 	}
 
-	public void acceptMoney() {
+	public void acceptMoney() { // allows user input for money
 		System.out.println("Please enter $" + currentPrice);
 		System.out.println("To enter a quarter, enter \"q\"");
 		System.out.println("To enter a dime, enter \"d\"");
@@ -72,9 +72,9 @@ class Machine {
 		addMoney();
 	}
 
-	public void addMoney() {
+	public void addMoney() { // subtracts inputted money from current price
 		String a = "Yes";
-		for (String x = a; x.equals("Yes"); x = a) {
+		for (String x = a; x.equals("Yes"); x = a) { // accepts money until done
 			if (currentMoneyType.equals(quarter)) {
 				currentInput = 0.25;
 				currentPrice = currentPrice - currentInput;
@@ -88,7 +88,7 @@ class Machine {
 				currentPrice = currentPrice - currentInput;
 				acceptMoney();
 			} else if (currentMoneyType.equals("done")) {
-				a = "No";
+				a = "No"; // stops the loop of accepting money
 			} else {
 				System.out.println("Not a valid input");
 				acceptMoney();
@@ -96,7 +96,7 @@ class Machine {
 		}
 	}
 
-	public void determineMoney() {
+	public void determineMoney() { // checks if enough money has been entered
 		if (currentPrice != 0 && currentPrice > 0) {
 			System.out.println("You have not entered enough money.");
 			acceptMoney();
@@ -106,7 +106,7 @@ class Machine {
 			System.out.println(currentDrink.name);
 			currentDrink.inventory = currentDrink.inventory - 1;
 		}
-		if (currentPrice < 0) {
+		if (currentPrice < 0) { // gives change if necessary
 			System.out.println("Here is your drink!");
 			System.out.println(currentDrink.name);
 			currentDrink.inventory = currentDrink.inventory - 1;
@@ -121,7 +121,7 @@ class Machine {
 		return goOn;
 	}
 
-	public void yesOrNo(String additionalQuestions) {
+	public void yesOrNo(String additionalQuestions) { // decides whether to keep running driver or not
 		if (additionalQuestions.equals("Yes")) {
 			goOn = "Yes";
 		}
