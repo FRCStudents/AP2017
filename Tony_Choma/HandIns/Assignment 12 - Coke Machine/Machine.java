8fd3a7509233f60cad25a051283146a16a1d401a
tchomes <tonychoma322@gmail.com>
2017-11-06 08:19:28 -0500

    Machine updates

--- Tony_Choma/HandIns/Assignment 12 - Coke Machine/Machine.java
+++ Tony_Choma/HandIns/Assignment 12 - Coke Machine/Machine.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Machine{
         String[] drinks = new String[]{"Coca Cola", "Sprite", "Pepsi", "Water", "Fanta"};
-        private String x;
+        private String input;
 
         public void returnTypes(){
                 for (int i = 0; i < drinks.length; i++){
@@ -14,13 +14,23 @@ public class Machine{
                 Scanner s = new Scanner(System.in);
                 System.out.println("What would you like to drink? ");
                 returnTypes();
-                x = s.nextLine();
-                return x;
+                input = s.nextLine();
+                return input;
+        }
+
+        public boolean checkInput(){
+                for(int x = 0; x < drinks.length; x++){
+                        if(input == drinks[x]){
+                                return true;
+                        }
+                }
+                return false;
         }
 
         public static void main(String[] args){
                 Machine m = new Machine();
                 m.userInput();
-
+                System.out.println(m.input);
+                m.checkInput();
         }
 }
