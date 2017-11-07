9dfe922d91c0ed83c183a7e7a82f8fad17fd6e67
Tom Howells <tomert42@gmail.com>
2017-11-07 12:10:56 -0500

    updating elevens with shuffle function and assorted improvements

--- Tom_Howells/HandIns/016Elevens/Elevens.java
+++ Tom_Howells/HandIns/016Elevens/Elevens.java
@@ -1,10 +1,11 @@
+import java.util.*;
+
 class Elevens {
     public static void main(String[] args) {
         Deck a = new Deck();
 
         for (int i = 0; i < 52; i++){
-            System.out.println(a.getCard(i));
+            System.out.println(a.getCard(i) + " " + a.getCard(i).getValue());
         }
-        System.out.print(a.getCard(51));
     }
 }
