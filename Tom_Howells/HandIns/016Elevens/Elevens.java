4da39b0eeb236f2aa2bb9c48824ca630dc71a18e
Tom Howells <tomert42@gmail.com>
2017-11-08 08:20:10 -0500

    elevens updates and final palindrome project

--- Tom_Howells/HandIns/016Elevens/Elevens.java
+++ Tom_Howells/HandIns/016Elevens/Elevens.java
@@ -4,8 +4,6 @@ class Elevens {
     public static void main(String[] args) {
         Deck a = new Deck();
 
-        for (int i = 0; i < 52; i++){
-            System.out.println(a.getCard(i) + " " + a.getCard(i).getValue());
-        }
+        
     }
 }
