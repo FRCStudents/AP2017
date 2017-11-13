2aecfaa555179f34b7a2ad6771b802ab71d4b1f0
glaliberte <CaveOrWhatever@users.noreply.github.com>
2017-11-13 12:39:42 -0500

    edit
    Edited card object

---
+++ Gareth_Laliberte/Assignment16/card.java
@@ -0,0 +1,28 @@
+public class Card{
+
+        public String getSuit(){
+        
+        }
+        
+        public int getNumber(){
+        
+        }
+        
+        public String getRank(){
+        
+        }
+        
+        public String setSuit(){
+        
+        }
+        
+        public int setNumber(){
+        
+        }
+        
+        public String setRank(){
+        
+        }
+        
+      
+}
