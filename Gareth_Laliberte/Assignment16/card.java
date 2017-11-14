5023b07cd0c42c5e9a6d13fea8548c210261eef4
glaliberte <CaveOrWhatever@users.noreply.github.com>
2017-11-14 13:23:35 -0500

    edit

--- Gareth_Laliberte/Assignment16/card.java
+++ Gareth_Laliberte/Assignment16/card.java
@@ -31,9 +31,10 @@ public class Card{
         
         public void displaySuit(){
             setSuit();
-            System.out.print(s);
+            System.out.print("Suit" + s);
         }
         
+        //not too sure about this.
         boolean isEqual(Card c){
             if((s && n) == (s && n)){
                 return true;
@@ -41,4 +42,15 @@ public class Card{
                 return false;
             }
         }
+        
+        public void printCard(){
+            System.out.println("Card");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-");
+            System.out.println("Rank: " + r);
+            System.out.println("Suit: " + s);
+            System.out.println("Number: " + n);
+        }
+        
+        boolean isHigher(){
+            
 }
