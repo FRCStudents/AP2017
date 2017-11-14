9b2e29a9d09fb0b07dfbdbdbaeca9a74a220efb3
glaliberte <CaveOrWhatever@users.noreply.github.com>
2017-11-14 13:06:33 -0500

    edit

--- Gareth_Laliberte/Assignment16/card.java
+++ Gareth_Laliberte/Assignment16/card.java
@@ -18,16 +18,27 @@ public class Card{
         }
         
         public void setSuit(String s){
-        
+            Suit = s;
         }
         
         public void setNumber(int n){
-        
+            Number = n;
         }
         
         public void setRank(String r){
-        
+            Rank = r;
         }
         
-      
+        public void displaySuit(){
+            setSuit();
+            System.out.print(s);
+        }
+        
+        boolean isEqual(Card c){
+            if((s && n) == (s && n)){
+                return true;
+            } else {
+                return false;
+            }
+        }
 }
