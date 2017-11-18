cdb82cde1698684b2f02f4c2eb80c1aa4f8a561c
James Dimino <JamesDimino01@gmail.com>
2017-11-18 12:19:25 -0500

    Removing File

---
+++ James_Dimino/Handins/016Cards/Deck.java
@@ -0,0 +1,18 @@
+public class Deck{
+	
+	Card[] deck;
+
+		public getDeck(){
+			return deck;
+		}
+
+		public void setDeck(int numOfCards){
+			deck[] = numOfCards;
+		}
+
+		public void displayDeck(){
+			System.out.println(deck[]);
+		}
+
+
+}
