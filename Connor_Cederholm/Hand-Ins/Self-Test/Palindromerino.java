9e7cf16a768abd926035aab759f40d5492de6cec
CjCederholm <connorjcederholm@gmail.com>
2017-11-17 23:37:46 -0500

    Uploading Deck at 11:37 PM EST

--- Connor_Cederholm/Hand-Ins/Self-Test/Palindromerino.java
+++ Connor_Cederholm/Hand-Ins/Self-Test/Palindromerino.java
@@ -1,16 +1,16 @@
-public class Palindromerino{
+class Palindromerino{
 String string;
 public String setString(String s){
 	string = s;
 	return string;
 }
 public String palindromer(){
-for (int i = 0; i <= string.length(); i++){
-	if (string.charAt(i) == string.charAt(string.length() - i)){
+for (int i = 0; i < string.length(); i++){
+	if (string.charAt(i) == string.charAt(string.length() - i-1)){
 	return "Palindrome, BABY";
 	}
-}
-	return "no, idiot";
-	
+	return "No palindrome here";
+	}
+	return " ";
 }
 }
