47ab53ca4154bc1aec8242f6221bd534e0d814d0
Efraim Krug <ekrug@foxboroughrcs.org>
2017-11-19 15:32:26 -0500

    Changing"

--- Connor_Cederholm/Hand-Ins/Self-Test/PalindromerinoDriver.java
+++ Connor_Cederholm/Hand-Ins/Self-Test/PalindromerinoDriver.java
@@ -2,7 +2,11 @@ class PalindromerinoDriver{
 	public static void main (String [] argv){
 		Palindromerino p = new Palindromerino ();
 
-		p.setString("raccar");
+		p.setString("raxccar");
+		System.out.println(p.palindromer());
+		p.setString("Racecar");
+		System.out.println(p.palindromer());
+		p.setString("This is wrongT");
 		System.out.print(p.palindromer());
 	}
 }
