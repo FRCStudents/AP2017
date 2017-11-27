7c4305949a607cbd948a32237acd535dee68af28
Archit Sahay <ArchitSahay>
2017-11-27 13:43:37 -0500

    Adding all my files! yay!

---
+++ Archit_Sahay/HandIns/Assign03-EggObject/TheEgg.java
@@ -0,0 +1,11 @@
+public class Egg{
+    boolean fertilized;
+    String color;
+    String current_color= "blue";
+    String getcurrent_color () {
+    		return current_color;
+    }
+    void setcurrent_color(String newcolor) {
+    	current_color=newcolor;
+    }
+   String 
--- Archit_Sahay/HandIns/Assign08-Resort/ResortDriver.java
+++ Archit_Sahay/HandIns/Assign08-Resort/ResortDriver.java
@@ -3,7 +3,11 @@ public class ResortDriver{
 
     Resort r = new Resort();
     r.temp = 81;
+<<<<<<< HEAD:Archit_Sahay/HandIns/Assignment03/ResortDriver.java
+    System.out.println(r.activity ());
+=======
     System.out.println(r.activity());
+>>>>>>> b6c087e8f2afc8a718afb4cefc7a877b812149ea:Archit_Sahay/HandIns/Assign08-Resort/ResortDriver.java
 
     }
 }
