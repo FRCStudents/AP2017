41f7546b55744df4374d3351ddaef2be62495969
Ademirs <alexdemirs@gmail.com>
2017-11-28 07:47:52 -0500

    Adding tictac

--- Alex_Demirs/classwork/017TicTac/TicTacToe.java
+++ Alex_Demirs/classwork/017TicTac/TicTacToe.java
@@ -1,19 +1,20 @@
 public class TicTacToe{
+	String [][] board = new String [3][3];
 
-
-	char[][] x = newchar[][]{
-		{'x','o','x'}
-		{'o','o','o'}
-		{'x','o','x'}
+	public void setSquare(int col, int row, String value){
+		board [row][col] = value;
 	}
-	char[][] T = new char[3][3];
-
-
-
-
-	public char getValueofBox(int x, int y){
-		return T[x][y];
+	
+	
+	public void displayBoard(){
+		System.out.println(" " + board [0][0] + " | " + board [0][1] + " | " + board [0][2] + " " );
+		System.out.println("---+---+---");
+		System.out.println(" " + board [1][0] + " | " + board [1][1] + " | " + board [1][2] + " " );
+		System.out.println("---+---+---");
+		System.out.println(" " + board [2][0] + " | " + board [2][1] + " | " + board [2][2] + " " );
 	}
 
-
+	public String[][] getBoard(){
+		return board;
+	}
 }
---
+++ Alex_Demirs/classwork/018CoffeeShop/Coffee Cup/CoffeeCup.java
@@ -0,0 +1,14 @@
+java.ut
+public class CoffeeCup{
+
+List<List<Integer>> Cup = new ArrayList<List<Integer>>();
+
+Cup.add(Arrays.asList(0, 1, 0, 1, 0));
+Cup.add(Arrays.asList(0, 1, 1, 0, 1));
+Cup.add(Arrays.asList(0, 0, 0, 1, 0));
+
+
+
+
+
+}
