3126b15eca74cdf454f95d675e5d26a3a6e02bf6
tchomes <tonychoma322@gmail.com>
2017-12-01 09:07:28 -0500

    Palindrome changes

--- Tony_Choma/HandIns/Assignment 15 - Palindrome/Palindrome.java
+++ Tony_Choma/HandIns/Assignment 15 - Palindrome/Palindrome.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class Palindrome{
-        public static pchecker(String input){
-                
+        public static void pchecker(String input){
+                System.out.println(input);
         }
 
         public static void main(String[] args){
                 Palindrome p = new Palindrome();
                 Scanner s = new Scanner(System.in);
+                System.out.println("Check to see that this phrase is a palindrome: ");
                 String userInput = s.nextLine();
                 pchecker(userInput);
         }
