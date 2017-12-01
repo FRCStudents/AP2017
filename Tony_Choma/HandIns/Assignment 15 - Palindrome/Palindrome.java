6f01a51db318f8f366a7c69ceca4e5a31f0a228f
tchomes <tonychoma322@gmail.com>
2017-12-01 08:48:43 -0500

    Adding Palindrome.java

---
+++ Tony_Choma/HandIns/Assignment 15 - Palindrome/Palindrome.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Palindrome{
+        public static pchecker(String input){
+                
+        }
+
+        public static void main(String[] args){
+                Palindrome p = new Palindrome();
+                Scanner s = new Scanner(System.in);
+                String userInput = s.nextLine();
+                pchecker(userInput);
+        }
+}
