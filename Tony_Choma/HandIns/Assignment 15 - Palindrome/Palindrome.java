2cbd1e268b1eb26f5bbcfe2bf32a585a63d3a190
tchomes <tonychoma322@gmail.com>
2017-12-04 12:16:52 -0500

    Palindrome finished

--- Tony_Choma/HandIns/Assignment 15 - Palindrome/Palindrome.java
+++ Tony_Choma/HandIns/Assignment 15 - Palindrome/Palindrome.java
@@ -6,9 +6,9 @@ public class Palindrome{
         public void pconvert(String input){
                 String reverse = new StringBuffer(input).reverse().toString();
                 if(reverse.equalsIgnoreCase(input)){
-                        System.out.println("Yes");
+                        System.out.println("This phrase is a Palindrome");
                 }else{
-                        System.out.println("No");
+                        System.out.println("This phrase isn't a Palindrome");
                 }
 }
         public static void main(String[] args) throws java.lang.Exception {
