69c5010a0304710635b0b62909a2231d47709740
tchomes <tonychoma322@gmail.com>
2017-12-04 11:57:34 -0500

    CHAMELEON.JAVA

---
+++ Animals/Chameleon.java
@@ -0,0 +1,28 @@
+public class Chameleon{
+	private int weight = 180;
+	private int height = 4;
+	private String color = "Green";
+	private String sound;
+	private int speed;
+
+	public int getWeight(){
+		return weight;
+	}
+	public void setWeight(int newWeight){
+		weight = newWeight;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+	public void setHeight(int newHeight){
+		height = newHeight;
+	}
+
+	public String getColor(){
+		return color;
+	}
+	public void setColor(String newColor){
+		color = newColor;
+	}
+}
