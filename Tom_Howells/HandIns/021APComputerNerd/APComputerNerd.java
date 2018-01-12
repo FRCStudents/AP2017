abd25ff0fbb7794b035ccea1aae89e44041beeb7
Tom Howells <tomert42@gmail.com>
2018-01-12 10:10:20 -0500

    apcomputernerd w/o sort

--- Tom_Howells/HandIns/021APComputerNerd/APComputerNerd.java
+++ Tom_Howells/HandIns/021APComputerNerd/APComputerNerd.java
@@ -1,4 +1,4 @@
-abstract class APComputerNerd implements Comparable{
+class APComputerNerd implements Comparable{
     private int intelligence;
     private int speed;
     private int laptopPerformance;
@@ -43,11 +43,28 @@ abstract class APComputerNerd implements Comparable{
         firstName = fn;
     }
 
-    public int compareTo(Object o, int choice) { // choice = 0 for intelligence, choice = 1 for firstName
-        if (choice == 0) {
-            return 0;
-        } else if (choice == 1) {
-            return 1;
+    // return -1 if other is greater, 0 if equal, and 1 if this object is greater
+    public int compareTo(APComputerNerd nerd, boolean choice) { // choice = 0 for intelligence, choice = 1 for firstName
+        if (choice) {
+            if (nerd.getIntelligence() > intelligence) {
+                return -1;
+            } else if (nerd.getIntelligence() < intelligence) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (nerd.getFirstName().charAt(0) > firstName.charAt(0)) {
+                return -1;
+            } else if (nerd.getFirstName().charAt(0) < firstName.charAt(0)) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
+    }
+
+    public String toString() {
+        return (firstName + "(" + intelligence + "," + speed + "," + laptopPerformance + ")");
     }
 }
--- Tom_Howells/HandIns/021APComputerNerd/GoAPComputerNerd.java
+++ Tom_Howells/HandIns/021APComputerNerd/GoAPComputerNerd.java
@@ -10,9 +10,31 @@ class GoAPComputerNerd {
         arr[0] = new APComputerNerd();
         arr[1] = new APComputerNerd(3, 5, 2, "Rob");
         arr[2] = new APComputerNerd(5, 4, 8, "Aaron");
+        System.out.println("Unsorted:");
+        printArr();
+
+        sort(0);
+        System.out.println("Sorted by intelligence:");
+        printArr();
+
+        sort(1);
+        System.out.println("Sorted by first name:");
+        printArr();
     }
 
     public static void sort(int choice) { // 0 for intelligence, 1 for firstName
+        if (choice == 0) {
+            
+        } else {
 
+        }
+    }
+
+    public static void printArr() {
+        System.out.println("[");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.print(arr[arr.length - 1] + "]");
     }
 }
