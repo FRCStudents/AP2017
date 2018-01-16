f72cd9bd66dda4e64ac05c6944f950ae143a6124
tchomes <tonychoma322@gmail.com>
2018-01-16 12:01:29 -0500

    Updates on APCompNerd

---
+++ Tony_Choma/HandIns/Assignment 18 - APComputerNerd/CompNerd.java
@@ -0,0 +1,35 @@
+public class APCompNerd{
+        int intel;
+        int speed;
+        int laptopPerf;
+        private String firstName;
+
+//getters and setters for intel
+        public int getIntel(){
+                return intel;
+        }
+        public void setIntel(int newIntel){
+                intel = newIntel;
+        }
+//getters and setters for speed
+        public int getSpeed(){
+                return speed;
+        }
+        public void setSpeed(int newSpeed){
+                speed = newSpeed;
+        }
+//getters and setters for laptopPerf
+        public int getlaptopPerf(){
+                return laptopPerf;
+        }
+        public void setLaptopPerf(int newLaptopPerf){
+                laptopPerf = newLaptopPerf;
+        }
+//getters and setters for firstName
+        public String getFirstName(){
+                return firstName;
+        }
+        public void setFirstName(String newFirstName){
+                firstName = newFirstName;
+        }
+}
