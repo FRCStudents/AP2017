4872c83acaf71ba984653300617ccc4b12b6f3df
James Dimino <JamesDimino01@gmail.com>
2018-01-19 09:15:20 -0500

    Update

--- James_Dimino/Handins/020APCompNerd/APComputerNERD.java
+++ James_Dimino/Handins/020APCompNerd/APComputerNERD.java
@@ -4,27 +4,9 @@ public class APComputerNERD {
 	private int speed;
 	private int laptopPerformance;
 	private String firstName;
-
-	int testArr[] = {3, 5, 6, 2, 4};
-	int testArr2[] = {0, 0, 0, 0, 0};
-
+	
 	public APComputerNERD(){
 		//hmmmm.... constructor you say?
-	}
-
-	public void sort(){
-		int temp = 0;
-		for (int j = 0; j < testArr.length; j++){
-			int lastPlace = -1;
-				for (int i = 0; i < testArr.length; i++){
-					if (testArr[i] < temp){
-						temp = testArr[i];
-						lastPlace = i;
-						testArr2[j] = testArr[i];
-					}
-				}
-			testArr[lastPlace] = 999;	
-		}
 	}
 
 	public void printTest(){
--- James_Dimino/Handins/020APCompNerd/APComputerNERDDriver.java
+++ James_Dimino/Handins/020APCompNerd/APComputerNERDDriver.java
@@ -1,4 +1,23 @@
 public class APComputerNERDDriver {
+
+	static int testArr[] = {3, 5, 6, 2, 4};
+	static int testArr2[] = {0, 0, 0, 0, 0};
+
+	public static void sort(){
+		int temp = 999;
+		for (int j = 0; j < testArr.length; j++){
+			int lastPlace = -1;
+				for (int i = 0; i < testArr.length; i++){
+					if (testArr[i] < temp){
+						temp = testArr[i];
+						lastPlace = i;
+						testArr2[j] = testArr[i];
+					}
+				}
+			testArr[lastPlace] = 999;	
+		}
+	}
+
 	public static void main(String[] argv){
 
 	APComputerNERD a = new APComputerNERD();
