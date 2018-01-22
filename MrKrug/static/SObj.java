64d3e29fbed8ba4aeef301ba9b3725835d4b69a9
Efraim Krug <ekrug@foxboroughrcs.org>
2018-01-22 16:40:51 -0500

    Adding static int code

--- MrKrug/static/SObj.java
+++ MrKrug/static/SObj.java
@@ -1,7 +1,12 @@
 
 class SObj {
 	static int staticInt = 0;
+	// tracks how many SObj objects are created!
+	public static int SObjCount = 0;
+	public static int pubStatInt = 0;
+
 	public SObj(){
+		SObjCount++;
 		if(staticInt > 2) 
 			staticInt+=2 ;
 		else
@@ -14,5 +19,13 @@ class SObj {
 
 	public int getStaticInt(){
 		return staticInt;
+	}
+
+	public static int getPubStatInt(){
+		return pubStatInt;
+	}
+
+	public static void incPubStatInt(){
+		pubStatInt++;
 	}
 }
--- MrKrug/static/SObjGo.java
+++ MrKrug/static/SObjGo.java
@@ -17,5 +17,16 @@ class SObjGo {
 		System.out.println("so: " + so.getStaticInt());
 		System.out.println("so2: " + so2.getStaticInt());
 		System.out.println("so3: " + so3.getStaticInt());
+		System.out.println("------------------------------");
+		System.out.println("NOTICE: Some of these methods and attributes are");
+		System.out.println("being accessed via the class name - not the instance name!"); 
+			
+		System.out.println("-- set direct: ");
+		SObj.pubStatInt = 3;
+		System.out.println("Direct access: " + so3.pubStatInt);
+		SObj.incPubStatInt();
+		System.out.println("Even more direct: " + SObj.pubStatInt);
+		System.out.println("And Very direct: " + SObj.getPubStatInt());
+		System.out.println("By the way, we have " + SObj.SObjCount + " SObj objects!");
 	}
 }
