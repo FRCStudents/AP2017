5208db4d5777e6edaf6421d6d7638a43d895323d
James Dimino <JamesDimino01@gmail.com>
2018-01-23 03:10:00 -0500

    Science Fair

---
+++ James_Dimino/Science Fair/ScienceFair2018Driver.java
@@ -0,0 +1,16 @@
+public class ScienceFair2018Driver {
+	public static void main(String[] argv){
+
+	ScienceFair2018 s = new ScienceFair2018();
+
+	s.question0();
+	s.question1();
+	s.question2();
+	s.question3();
+	s.question4();
+	s.clearPage();
+
+	s.theFuckingBrains();
+
+	}
+}
