38716fb8c2ad71d85f0f9fb0e9f8157d0841a7bd
Svengat <sudish.vengat@gmail.com>
2018-01-31 10:50:37 -0500

    Adding Files

---
+++ Sudish_Vengat/Handins/UnfinishedWork/APComputerNerdDriver.java
@@ -0,0 +1,46 @@
+public class APComputerNerdDriver{
+
+	public static APComputerNerd[] array = new APComputerNerd[3];
+
+	
+	public static void swap(int i, int p){
+		APComputerNerd placeholder = array[i];
+		array[i] = array[p];
+		array[p] = placeholder;
+	}
+
+	
+	public static void main(String[] args) {
+
+        array[0] = new APComputerNerd();
+        array[1] = new APComputerNerd(4, 8, 7, "Dwight");
+        array[2] = new APComputerNerd(2, 3, 6, "Michael");
+        System.out.println("Unsorted Nerds:");
+        printArray();
+
+        sort(0);
+        System.out.println("These Nerds are sorted by their intelligence:");
+        printArr();
+
+        sort(1);
+        System.out.println("These Nerds are sorted by their first name:");
+        printArray();
+    }
+
+	
+	public static void sort(int choice) {
+        if (choice == 0) {
+        	return "choice: "
+        } else {
+        	return "error"
+        }
+    }
+
+    public static void printArray() {
+        System.out.println("[");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+        System.out.print(arr[arr.length - 1] + "]");
+    }
+}
---
+++ Sudish_Vengat/Handins/UnfinishedWork/WorldFlow/ComradeData.java
@@ -0,0 +1,12 @@
+public class ComradeData {
+
+	public String html1 "<table frame = "box"> <tr> <td></td> <td></td> <td></td> <td></td> <td></td> </tr>"
+	public String html2 "<tr> <td></td> <td></td> <td></td> <td></td> <td></td> </tr>"
+	public String html3 "<tr> <td></td> <td></td> <td></td> <td></td> <td></td> </tr>"
+	public String html4 "<tr> <td></td> <td></td> <td></td> <td></td> <td></td> </tr>"
+
+
+
+}
+
+// <link href = "" text = "text/css" rel = "stylesheet">
