ac564b495d7c6b34a97a7186714c78360fb69c6e
Ademirs <alexdemirs@gmail.com>
2018-02-01 11:39:45 -0500

    fixing nerd

--- Alex_Demirs/classwork/021APComputerNERD/APCNGO.java
+++ Alex_Demirs/classwork/021APComputerNERD/APCNGO.java
@@ -1,18 +1,18 @@
 import java.util.*;
-public class APComputerNERDDriver{
-		private static APComputerNERDDriver[] arrayOfNERDZ = new APComputerNERDDriver[16];
+public class APCNGO{
+		private static APComputerNERD[] arrayOfNERDZ = new APComputerNERD[16];
 		private static String[] classmates = { "Marx", "Putin", "Lenin", "Stalin", "Malenkov", "Khrushchev", "Brezhnev",
 							"Andropov", "Chernenko", "Gorbachev", "Yanayev", "Kamenev", "Zinoviev", "Beria", "Alex", "Molotov" };
 							
-		private static void setArraySortType(boolean thing){
+		private static void setArraySortType(boolean flagger){
 			for(int i=0; i < arrayOfNERDZ.length; i++){
-				if (thing){ arrayOfNERDZ[i].setSortTypeName(); } 
+				if (flagger){ arrayOfNERDZ[i].setSortTypeName(); } 
 					else { arrayOfNERDZ[i].setSortTypeIntelligence(); }
 			}
 		}	
 	
 		public static void sortArray(){
-			APComputerNERDDriver yada;
+			APComputerNERD yada;
 			for(int x = 0; x < arrayOfNERDZ.length; x++)
 				for(int y = 0; y < arrayOfNERDZ.length; y++){
 					if(arrayOfNERDZ[x].compareTo(arrayOfNERDZ[y]) < 0){
@@ -24,16 +24,16 @@ public class APComputerNERDDriver{
 		}
 	
 		public static void fillArray(){
-			int x; //intelegence level
-			int y; //speed
-			int z; //laptop performance
+			int x; 
+			int y; 
+			int z; 
 
 			for (int i = 0; i < arrayOfNERDZ.length; i++){
-				x = ((int) Math.floor(Math.random() * 10) + 1); //number between 1-10
-				y = ((int) Math.floor(Math.random() *  5) + 1); //number between 1-5
-				z = ((int) Math.floor(Math.random() * 20) + 1); //number between 1-20
+				x = ((int) Math.floor(Math.random() * 10) + 1);
+				y = ((int) Math.floor(Math.random() *  5) + 1); 
+				z = ((int) Math.floor(Math.random() * 20) + 1); 
 
-				arrayOfNERDZ[i] = new APCN(classmates[i], x, y, z);
+				arrayOfNERDZ[i] = new APComputerNERD(classmates[i], x, y, z);
 			}
 		}
  
@@ -42,26 +42,26 @@ public class APComputerNERDDriver{
 		}
 
 		public static void main(String[] args){ 
-			APCN k = new APCN();
+			APComputerNERD k = new APComputerNERD();
 			k.displayKevin();
 			
 			fillArray();
 			
-			System.out.println("\n\n++++++++ Original Array Of AP Computer Nerds ++++++++");
+			System.out.println("\n\n-------- Original Array Of AP Computer Nerds --------");
 			showArray();
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+			System.out.println("--------------------------------------------------");
 			
-			System.out.println("\n\n++++++++ Sorted Alphabetically by Name ++++++++");
+			System.out.println("\n\n-------- Sorted Alphabetically by Name ----------");
 			setArraySortType(true);
 			sortArray();
 			showArray();
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
+			System.out.println("-------------------------------------------------");
 	        
 			setArraySortType(false);
 	        sortArray();
-			System.out.println("\n\n++++++++ Sorted Least to Greatest on Intelegence Level ++++++++");
+			System.out.println("\n\n+------- Sorted Least to Greatest on Intelegence Level --------");
 	        showArray();
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+			System.out.println("---------------------------------------------------------------");
 
 		}	
 	}
