8d4d4157d4ce117bd46f5e35941296c80927dc91
Ademirs <alexdemirs@gmail.com>
2018-02-07 14:56:54 -0500

    adding the classwork probs

---
+++ Alex_Demirs/Discord Problems/file1.java
@@ -0,0 +1,19 @@
+public class file1{
+
+
+  static String yell = "Yo, Dudele!";
+
+
+public static void yeller(){
+	System.out.println(yell);
+}
+
+  public static void main (String [] GoBaby){
+
+  	yeller();
+
+  }
+
+	
+
+}
