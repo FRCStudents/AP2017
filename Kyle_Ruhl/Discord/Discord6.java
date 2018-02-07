d596569b76c684dac940a42a9f705dd43216f4d4
KyleRuhl <kylethe355@gmail.com>
2018-02-07 18:42:55 -0500

    ++ Completed Discord Problems from 2/7

---
+++ Kyle_Ruhl/Discord/Discord6.java
@@ -0,0 +1,12 @@
+public class Discord6{
+
+private int singleAttribute;
+
+	public Discord6(){
+		this.singleAttribute = 17;
+	}
+
+	public String toString(){
+		return ("The Single Attribute's Value is: " + singleAttribute);
+	}
+}
