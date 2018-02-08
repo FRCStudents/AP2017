aef03d5e6b4b0a12a0dbecef23c38bc63a1c0626
Ademirs <alexdemirs@gmail.com>
2018-02-08 10:19:43 -0500

    fixing 3 and 5

--- Alex_Demirs/Discord Problems/file3.java
+++ Alex_Demirs/Discord Problems/file3.java
@@ -1,14 +1,14 @@
 public class file3{
 
-public static String thing = "hecking hecking my computer is so slow";
 
 
-public static void connorJustSaid(){
-	System.out.print("This is the \"string\", you know: " + thing);
+
+public static void connorJustSaid(String x){
+	System.out.print("This is the \"string\", you know: " + x);
 }
 
 public static void main (String [] GoBaby){
-	connorJustSaid();
+	connorJustSaid("hecking hecking my computer is so slow");
 }
 
 }
