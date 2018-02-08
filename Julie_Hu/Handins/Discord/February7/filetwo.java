0c2f35dd7fd6fe3b20836b9e73d92178feba3671
hu10julie <hu10julie@gmail.com>
2018-02-08 10:17:38 -0500

    Discord Problems

---
+++ Julie_Hu/Handins/Discord/February7/filetwo.java
@@ -0,0 +1,8 @@
+//Question 2: Write a function that prints a line of "*"
+
+public class filetwo{
+	public static void main(String[] argv){
+		for (int i=0;i<=10; i++)
+			System.out.print("*");
+}
+}
