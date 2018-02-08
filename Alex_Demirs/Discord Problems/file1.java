854b42b76be009aedfcd4ec6daaeada809d3fa8d
Ademirs <alexdemirs@gmail.com>
2018-02-08 10:22:58 -0500

    fix 1

--- Alex_Demirs/Discord Problems/file1.java
+++ Alex_Demirs/Discord Problems/file1.java
@@ -1,16 +1,8 @@
 public class file1{
 
-
-  static String yell = "Yo, Dudele!";
-
-
-public static void yeller(){
-	System.out.println(yell);
-}
-
   public static void main (String [] GoBaby){
 
-  	yeller();
+  	System.out.print("Yo, Dudele!");
 
   }
 
