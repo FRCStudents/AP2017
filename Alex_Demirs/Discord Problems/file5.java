bf362fd9c166ac50e3b3cd2af15212ae06f10057
Ademirs <alexdemirs@gmail.com>
2018-02-07 21:52:01 -0500

    making file5 great

--- Alex_Demirs/Discord Problems/file5.java
+++ Alex_Demirs/Discord Problems/file5.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class file5{
 
 public static void yayOrNay(boolean x){
@@ -11,31 +13,38 @@ if(x == true){
 public static boolean check(String y){
 boolean x;
 
-	if(y.charAt(0) == 'a'){
+	if(y.charAt(0) == 'a' || y.charAt(0) == 'A'){
 		return x = true;
 	}
 
-	if(y.charAt(0) == 'e'){
+	if(y.charAt(0) == 'e' || y.charAt(0) == 'E'){
 		return x = true;
 	}
 
-	if(y.charAt(0) == 'i'){
+	if(y.charAt(0) == 'i' || y.charAt(0) == 'I'){
 		return x = true;
 	}
 
-	if(y.charAt(0) == 'o'){
+	if(y.charAt(0) == 'o' || y.charAt(0) == 'O'){
 		return x = true;
 	}
 
-	if(y.charAt(0) == 'u'){
+	if(y.charAt(0) == 'u' || y.charAt(0) == 'U'){
 		return x = true;
 	}
 	return false;
 	}
 
 
+public static String stringGetter(){
+	Scanner imput = new Scanner(System.in); 
+	System.out.println("String please");
+	String Imput = imput.nextLine();
+	return Imput;
+}
+
 public static void main (String [] GoBaby){
 	String thing = "cpple";
-	yayOrNay(check(thing));
+	yayOrNay(check(stringGetter()));
 }
 }
