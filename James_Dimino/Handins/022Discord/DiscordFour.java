43952d83d1b8f604f4400bd391788c0abca49fcd
James Dimino <JamesDimino01@gmail.com>
2018-02-08 10:22:59 -0500

    Adding Discord Work

---
+++ James_Dimino/Handins/022Discord/DiscordFour.java
@@ -0,0 +1,17 @@
+public class DiscordFour {
+
+	public static void checkDivisble(int n) {
+		if (n % 6 == 0) {
+			System.out.println("Hah ha 6-divisor dude");
+		}
+
+		else {
+			System.out.println("(Bummer and a half)");
+		}
+	}
+	public static void main(String[] argv){
+
+	checkDivisble(12);
+
+	}
+}
---
+++ James_Dimino/Handins/022Discord/DiscordThree.java
@@ -0,0 +1,11 @@
+public class DiscordThree {
+
+	public static void printString(String s) {
+		System.out.println("This is the \"String\", you know: " + s);
+	}
+	public static void main(String[] argv){
+
+	printString("Heck To The Yeah");
+
+	}
+}
