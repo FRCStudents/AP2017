67588b522bc1d27da623832289e211bdec4a1beb
Nicole Sorial <grace.cola@gmail.com>
2018-02-08 11:05:22 -0500

    adding files 7 - 9 from discord activities

---
+++ Nicole_Sorial/HandIn/Discord/Feb7/File9a.java
@@ -0,0 +1,11 @@
+public class File9a {
+
+	public static void printStarLines() {
+		File9b f = new File9b();
+		f.printStars();
+	}
+
+	public static void main(String[] argv) {
+		printStarLines();
+	}
+}
