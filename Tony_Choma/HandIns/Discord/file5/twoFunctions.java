2aa20e6317ff0cf7ba15f08d5c193a73d38186e7
tchomes <tonychoma322@gmail.com>
2018-02-09 11:56:13 -0500

    file6 and file 7

--- Tony_Choma/HandIns/Discord/file5/twoFunctions.java
+++ Tony_Choma/HandIns/Discord/file5/twoFunctions.java
@@ -1,7 +1,6 @@
 public class twoFunctions{
-      String[] vowels = {"e" , "i", "o", "u", "a", "A", "E", "O", "U", "I"};
+      String[] vowels = {"e", "i", "o", "u", "a", "A", "E", "O", "U", "I"};
       boolean check;
-
       public void printString(){
             if(check == true){
                   System.out.println("Whew");
@@ -10,23 +9,21 @@ public class twoFunctions{
             }
       }
 
-      public void checkVowel(String s){
-            for(int i = 0; i < 1; i++){
-                  for(int x = 0; x < vowels.length - 1; x++){
-                        if(vowels[x].equals(s.charAt(i))){
-                              check = true;
-                        }else{
-                              check = false;
-                        }
+      public boolean checkVowel(String s){
+            for(int i; i < vowels.length; i++){
+                  if(vowels[0].equals(s.charAt(0))){
+                        return check = true;
+                  }else{
+                        return check = false;
                   }
             }
       }
 
       public static void main(String[] args){
             twoFunctions tF = new twoFunctions();
-            tF.checkVowel("Hello");
-            tF.printString();
-            tF.checkVowel("Apples");
+            //tF.checkVowel("Hello");
+            //tF.printString();
+            tF.checkVowel("epples");
             tF.printString();
       }
 }
