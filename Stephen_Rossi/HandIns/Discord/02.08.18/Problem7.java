cc8c01f70e0e149fd3fbef8a15345a18968d73a6
Stephen Rossi <stephenrossi99@gmail.com>
2018-02-09 11:27:17 -0500

    Yestreday's work

---
+++ Stephen_Rossi/HandIns/Discord/02.08.18/Problem7.java
@@ -0,0 +1,15 @@
+class Problem7 {
+	private int number;
+
+	public int getNumber(){
+		return number;
+	}
+
+	public Problem7(int number){
+		this.number = number;
+	}
+	
+	public Problem7(){
+		number = 17;
+	}
+}
---
+++ Stephen_Rossi/HandIns/Discord/02.08.18/Problem8.java
@@ -0,0 +1,18 @@
+class Problem8 {
+	private int number;
+
+	public int getNumber(){
+		return number;
+	}
+
+	public Problem8(int number){
+		this.number = number;
+	}
+	
+	public Problem8(){
+		number = 17;
+	}
+	public String toString(){
+		return "" + number;
+	}
+}
