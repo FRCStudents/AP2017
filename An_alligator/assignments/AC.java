357d3a70cdb4404050d1676da0d31ac3d77c5ce9
manradical <tunjikusimo@gmail.com>
2018-02-12 16:30:14 -0500

    wowowowowoo

---
+++ An_alligator/assignments/AC.java
@@ -0,0 +1,18 @@
+class AC{
+	int AC0;
+	int AC1;
+
+	public void prt(int x){
+		System.out.println(x);
+	}
+
+	public void print(String x){
+		System.out.println(x);
+	}
+
+	public void turnOnAC(){
+	}
+
+	public void turnOffAC(){
+	}
+}
---
+++ An_alligator/assignments/tDriver.java
@@ -0,0 +1,10 @@
+class tDriver{
+	public static void main(String[] args){
+		littleAC x = new littleAC();
+		bigAC y = new bigAC();
+		x.prt(x.AC0);
+		x.prt(x.AC1);
+		y.prt(y.AC0);
+		y.prt(y.AC1);
+	}
+}
