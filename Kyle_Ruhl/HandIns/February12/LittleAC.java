819b4f8ab411cd111b3ca6dff8e66bf454762067
Kyle <kylethe355@gmail.com>
2018-02-12 13:33:55 -0500

    Adding INCOMPLETE discord from Feb 12

---
+++ Kyle_Ruhl/HandIns/February12/LittleAC.java
@@ -0,0 +1,19 @@
+public class LittleAC extends AC{
+
+/*public LittleAC(){
+super();
+}
+*/
+//@overide
+protected void turnOnAC(){
+//attribute1 = newAttribute1;
+attribute1 = 1;
+}
+
+//@overide
+protected void turnOffAC(){
+//attribute2 = newAttribute2;
+attribute2 = 0;
+}
+
+}
