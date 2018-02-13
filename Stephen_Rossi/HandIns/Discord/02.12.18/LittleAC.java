070afab08ce438cecc1a70de3b99020225be7191
Stephen Rossi <stephenrossi99@gmail.com>
2018-02-13 08:31:29 -0500

    commit

---
+++ Stephen_Rossi/HandIns/Discord/02.12.18/LittleAC.java
@@ -0,0 +1,14 @@
+class LittleAC extends AC {
+
+	public LittleAC(){
+		ac = 0;
+	}
+
+	public void turnOnAC(){
+		ac = 1;
+	}
+
+	public void turnOffAC(){
+		ac = 0;
+	}
+}
---
+++ Stephen_Rossi/HandIns/Discord/02.12.18/VeryBigAC.java
@@ -0,0 +1,14 @@
+class VeryBigAC extends BigAC {
+
+	public VeryBigAC(){
+		ac = -100;
+	}
+
+	public void turnOnAC(){
+		ac = 100;
+	}
+
+	public void turnOffAC(){
+		ac = -100;
+	}
+}
