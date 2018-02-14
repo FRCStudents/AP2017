dffdb1e22ebf6b2797ac28aa3f8daf10e3a704e8
hu10julie <hu10julie@gmail.com>
2018-02-13 19:16:57 -0500

    Adding Discord Problems

---
+++ Julie_Hu/Handins/Discord/February7/fileeightb.java
@@ -0,0 +1,23 @@
+public class fileeightb{
+
+public static int number;
+
+public fileeightb(int x){
+number = x;
+}
+
+public fileeightb(){
+number = 17;
+}
+
+public String toString(){
+	return ("Value: " + number);
+}
+
+public static void main(String[] args){
+fileeightb r = new fileeightb();
+System.out.println(r);
+
+}
+
+}
---
+++ Julie_Hu/Handins/Discord/February7/filenineb.java
@@ -0,0 +1,13 @@
+public class filenineb{
+
+public static void printStarLines(){
+	fileninea a = new fileninea();
+	a.printStars();
+}
+
+public static void main(String[] args){
+	printStarLines();
+}
+
+
+}
